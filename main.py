d9219590c62f3a3b9705e7a2ccbd670f7cefb217
shoval-cohen <shoval835@gmail.com>
2021-12-29 20:50:17 +0200

    Worked until 537

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import cv2
 import h5py
-import imutils
 import numpy as np
 
 file_path = "./resources/SynthText.h5"
 db = h5py.File(file_path, 'r')
 
 im_names = list(db["data"].keys())
-image_num = 1
-for im in im_names:
 
-    print(f"working on image {image_num} with {im} name")
-    # for index in np.random.choice(973, 5):
-    # for index in [200]:
-    # im = im_names[index]
+# for index in range(1, 974):
+if True:
+    index = 537
+    im = im_names[index]
+    print(f"working on image {index} with name {im}")
 
     img = db['data'][im][:]
+    # cv2.imshow("Image", img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     font = db['data'][im].attrs['font']
     txt = db['data'][im].attrs['txt']
@@ -23,14 +24,12 @@ for im in im_names:
     wordBB = db['data'][im].attrs['wordBB']
 
     pts = np.swapaxes(charBB, 0, 2)
-    #
-    # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    # print(pts)
-    # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    for char in pts:
-        c = 0
+
+    for idx, char in enumerate(pts):
+        print(f"index is {idx}")
         original_char = char.copy()
         char = np.asarray(char, np.int32)
+        char = np.where(char > 0, char, 0)
         orig = cv2.polylines(img.copy(), np.asarray([char]), True, color=(0, 0, 255))
         # cv2.imshow("original", orig)
 
@@ -48,43 +47,42 @@ for im in im_names:
 
         ## (3) do bit-op
         char_image = cv2.bitwise_and(cropped, cropped, mask=mask)
-        c += 1
-        if char_image is not None:
-            # Check about use contour
-            # cv2.imshow("a", char_image)
-            char_image = cv2.GaussianBlur(char_image, (3, 3), 1)
-            char_image = cv2.Canny(char_image, 100, 200)
-            char_image = cv2.resize(char_image, (char_image.shape[1] * 4, char_image.shape[0] * 4))
-            # Add the char letter here
-            cv2.imwrite(f"./imgs/image_{image_num}_{c}.png", char_image)
 
+        # Check about use contour
+        # cv2.imshow("a", char_image)
+        char_image = cv2.GaussianBlur(char_image, (3, 3), 1)
+        char_image = cv2.Canny(char_image, 100, 200)
+        char_image = cv2.resize(char_image, (char_image.shape[1] * 4, char_image.shape[0] * 4))
+        # Add the char letter here
+        cv2.imwrite(f"./imgs/image_{index}_{idx}.png", char_image)
 
-            # ## Rotate without cutoff
-            #
-            # # calc angle
-            # x0, y0 = original_char[0]
-            # x1, y1 = original_char[1]
-            # x3, y3 = original_char[-1]
-            #
-            # cv2.circle(orig, (int(x0), int(y0)), 8, color=(0, 255, 0))
-            # cv2.circle(orig, (int(x1), int(y1)), 8, color=(0, 180, 0))
-            # cv2.circle(orig, (int(x3), int(y3)), 8, color=(0, 90, 0))
-            # cv2.imshow("original", orig)
-            #
-            # horizontal_angle = np.rad2deg(np.arctan((y1 - y0) / (x1 - x0))) if x1 != x0 else 0
-            # vertical_angle = np.rad2deg(np.arctan((x0 - x3) / (y0 - y3))) if y0 != y3 else 0
-            # print("horizontal_angle")
-            # print(horizontal_angle)
-            # print("vertical_angle")
-            # print(vertical_angle)
-            # angle = max(horizontal_angle, vertical_angle, key=abs)
-            # print("angle")
-            # print(angle)
-            #
-            # rotated = imutils.rotate_bound(char_image, angle)
-            # cv2.imshow(f"Rotated (Correct {angle})", rotated)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-    image_num += 1
+        # ## Rotate without cutoff
+        #
+        # # calc angle
+        # x0, y0 = original_char[0]
+        # x1, y1 = original_char[1]
+        # x3, y3 = original_char[-1]
+        #
+        # cv2.circle(orig, (int(x0), int(y0)), 8, color=(0, 255, 0))
+        # cv2.circle(orig, (int(x1), int(y1)), 8, color=(0, 180, 0))
+        # cv2.circle(orig, (int(x3), int(y3)), 8, color=(0, 90, 0))
+        # cv2.imshow("original", orig)
+        #
+        # horizontal_angle = np.rad2deg(np.arctan((y1 - y0) / (x1 - x0))) if x1 != x0 else 0
+        # vertical_angle = np.rad2deg(np.arctan((x0 - x3) / (y0 - y3))) if y0 != y3 else 0
+        # print("horizontal_angle")
+        # print(horizontal_angle)
+        # print("vertical_angle")
+        # print(vertical_angle)
+        # angle = max(horizontal_angle, vertical_angle, key=abs)
+        # print("angle")
+        # print(angle)
+        #
+        # rotated = imutils.rotate_bound(char_image, angle)
+        # cv2.imshow(f"Rotated (Correct {angle})", rotated)
 
     # words = np.swapaxes(np.array(wordBB, np.int32), 0, 2)
 
--- try.py
+++ try.py
@@ -1,4 +1,6 @@
-import numpy as np
+from keras.preprocessing import image
+import cv2
+import matplotlib.pyplot as plt
 
-a = np.zeros((2,5))
-
+img = image.load_img('15f8U.png', grayscale=True, target_size=(224, 224))
+img = image.img_to_array(img, dtype='uint8')
